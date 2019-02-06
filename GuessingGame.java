81e163c376a1c48cb22cebba7a0289fbba458eed
nicholasventura-tcsn <47402398+nicholasventura-tcsn@users.noreply.github.com>
2019-02-06 18:01:14 -0500

    Create GuessingGame.java

---
+++ GuessingGame.java
@@ -0,0 +1,25 @@
+/**
+ * This is a simple guess the number game
+ *
+ * Nicholas Ventura
+ * 2/6/19
+ */
+import java.util.Scanner;
+public class GuessingGame
+{
+    public static void main(String[] args) {
+        int num; 
+        int guess;
+        int guesses;
+        
+        String above = "Your guess was higher than the number! Try again or press 0 if you want to exit.";
+        String below = "Your guess was lower than the number! Try again or press 0 if you want to exit.";
+        
+        //Instructions
+        System.out.println("This a guessing game. You will have to guess a number from 1 to 100. Good luck!");
+    
+    
+    
+    
+    }
+}
