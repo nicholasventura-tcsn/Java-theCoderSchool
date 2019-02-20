eb37757551498d80e255228edd4e607b1ae9e231
nicholasventura-tcsn <47402398+nicholasventura-tcsn@users.noreply.github.com>
2019-02-20 18:02:53 -0500

    Update GuessingGame.java

--- GuessingGame.java
+++ GuessingGame.java
@@ -11,14 +11,40 @@ public class GuessingGame
         int num; 
         int guess;
         int guesses;
-        
-        String above = "Your guess was higher than the number! Try again or press 0 if you want to exit.";
-        String below = "Your guess was lower than the number! Try again or press 0 if you want to exit.";
+        boolean quit = false;
+        String above = "Your guess was higher than the number! Try again or press 0 if you want to exit or the quick way Alt F4.";
+        String below = "Your guess was lower than the number! Try again or press 0 if you want to exit or the quick way Alt F4.";
         
         //Instructions
         System.out.println("This a guessing game. You will have to guess a number from 1 to 100. Good luck!");
-    
-    
+        
+        num =(int) (Math.random()*100+1);
+        guesses = 0;
+        
+        Scanner scanGuess = new Scanner(System.in);
+        guess = scanGuess.nextInt();
+        guesses = 1;
+        while (guess != num && !quit) {
+            if (guess == 0) {
+                quit = true;
+                break;
+            }
+            if (guess > num) {
+                System.out.println(above);
+            } else if (guess < num) {
+                System.out.println(below); 
+            
+            
+            }  
+           
+        guess = scanGuess.nextInt();
+        guesses++;
+    }
+    if (guess == num) {
+        System.out.println("Congratulations! You guessed the right number it was " +num+ ". It took you " +guesses+ " guesses");
+    } else if (quit) {
+        System.out.println("Wow you actually did Alt F4 have fun waiting for your computer to restart");
+    }
     
     
     }
